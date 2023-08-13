be50c9ae2061e2d62b106d5576817e37962bcb5c
dzaba1 <blazej.zabialowicz@gmail.com>
2023-08-13 14:20:16 +0200

    Some model

---
+++ Frontend/src/model/match.ts
@@ -0,0 +1,8 @@
+export interface Match {
+    id?: number;
+    dateTime: Date;
+    leftPlayer: string;
+    rightPlayer: string;
+    leftPlayerScore: number;
+    rightPlayerScore: number;
+}
---
+++ Frontend/src/model/playerRating.ts
@@ -0,0 +1,4 @@
+export interface PlayerRating {
+    player: string;
+    rating: number;
+}
